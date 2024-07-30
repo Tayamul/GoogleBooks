e11fe894ae2fe0bfd3afbdb7a62ef57221312212
Tayamul <rai_tayamul@hotmail.com>
2024-07-30 15:51:30 +0100

    modify the existing index method to fetch and parse all data from the repository

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import models.DataModel
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request}
 import repositories.DataRepository
 
@@ -10,7 +12,10 @@ import scala.concurrent.{ExecutionContext, Future}
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository, implicit val ec: ExecutionContext) extends BaseController {
 
   def index(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    Future.successful(Ok(views.html.index()))
+    dataRepository.index().map {
+      case Right(item: Seq[DataModel]) => Ok {Json.toJson(item)}
+      case Left(error) => Status(error)(Json.toJson("Unable to find any books"))
+    }
   }
 
   def create = TODO
