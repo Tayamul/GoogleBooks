160225708f9fa9d0dfe2f5cfc429b4983553db2b
Tayamul <100432502+Tayamul@users.noreply.github.com>
2024-07-31 16:44:02 +0100

    Part 3 - Integration Testing (#4)
    * create a test data set of type DataModel for testing purposes
    
    * implement a test to ensure a book is stored into the database
    
    * implement a happy path test to retrieve a book from the database by its id
    
    * implement a test to ensure the delete action method functions as expected
    
    * implement a test to ensure updating data information is allowed
    
    * update the route to correctly handle creating new data

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,19 +1,33 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
+import models.DataModel
 import play.api.test.FakeRequest
 import play.api.http.Status
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{ControllerComponents, Result}
 import play.api.test.Helpers._
 import repositories.DataRepository
 
-import scala.concurrent.ExecutionContext
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
 
 class ApplicationControllerSpec extends BaseSpecWithApplication {
 
-  val TestApplicationController = new ApplicationController(
-    component,
-    repository,
-    executionContext
+  val TestApplicationController = new ApplicationController(component, repository)(executionContext)
+
+  private val dataModel: DataModel = DataModel(
+    "abcd",
+    "test name",
+    "test description",
+    100
+  )
+
+  private val updateDataModel: DataModel = DataModel(
+    "abcd",
+    "updated test name",
+    "updated test description",
+    150
   )
 
   "ApplicationController .index()" should {
@@ -27,17 +41,83 @@ class ApplicationControllerSpec extends BaseSpecWithApplication {
 
   "ApplicationController .create()" should {
 
+    "create a book in the database" in {
+
+      beforeEach()
+
+      val request: FakeRequest[JsValue] = buildPost("/api").withBody[JsValue](Json.toJson(dataModel))
+      val createdResult: Future[Result] = TestApplicationController.create()(request)
+
+      status(createdResult) shouldBe Status.CREATED
+
+      afterEach()
+
+    }
   }
 
   "ApplicationController .read()" should {
 
+    "find a book in the database by id" in {
+
+      beforeEach()
+
+      val request: FakeRequest[JsValue] = buildPost("/api").withBody[JsValue](Json.toJson(dataModel))
+      val createdResult: Future[Result] = TestApplicationController.create()(request)
+
+      status(createdResult) shouldBe Status.CREATED
+
+      val readResult: Future[Result] = TestApplicationController.read(dataModel._id)(FakeRequest())
+
+      status(readResult) shouldBe Status.OK
+      contentAsJson(readResult).as[JsValue] shouldBe Json.toJson(dataModel)
+
+      afterEach()
+
+    }
   }
 
   "ApplicationController .update()" should {
 
+    "update a book's field in the database by its id" in {
+
+
+    beforeEach()
+
+    val request: FakeRequest[JsValue] = buildPost("/api").withBody[JsValue](Json.toJson(dataModel))
+      val createdResult: Future[Result] = TestApplicationController.create()(request)
+
+      status(createdResult) shouldBe Status.CREATED
+
+      val updateRequest: FakeRequest[JsValue] = buildPut("/api/${dataModel._id}").withBody[JsValue](Json.toJson(updateDataModel))
+      val updateResult: Future[Result] = TestApplicationController.update(dataModel._id)(updateRequest)
+
+      status(updateResult) shouldBe Status.ACCEPTED
+
+    afterEach()
+
+    }
   }
 
   "ApplicationController .delete()" should {
 
+    "delete a book in the database by its id" in {
+
+    beforeEach()
+
+    val request: FakeRequest[JsValue] = buildPost("/api").withBody[JsValue](Json.toJson(dataModel))
+    val createdRequest: Future[Result] = TestApplicationController.create()(request)
+
+    status(createdRequest) shouldBe Status.CREATED
+
+    val deleteRequest: Future[Result] = TestApplicationController.delete(dataModel._id)(FakeRequest())
+
+    status(deleteRequest) shouldBe Status.ACCEPTED
+
+    afterEach()
+
+    }
   }
+
+  override def beforeEach(): Unit = await(repository.deleteAll())
+  override def afterEach(): Unit = await(repository.deleteAll())
 }
