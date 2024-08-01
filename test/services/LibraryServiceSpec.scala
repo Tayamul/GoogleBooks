5f01c69958a42679a0ce678dbc9f7a967cc14f0e
Tayamul <rai_tayamul@hotmail.com>
2024-08-01 18:57:31 +0100

    create a mock data to test different functionalities of the  LibraryService class

--- test/services/LibraryServiceSpec.scala
+++ test/services/LibraryServiceSpec.scala
@@ -5,6 +5,7 @@ import connectors.LibraryConnector
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.libs.json.{JsValue, Json}
 
 import scala.concurrent.ExecutionContext
 
@@ -13,4 +14,13 @@ class LibraryServiceSpec extends BaseSpec with MockFactory with ScalaFutures wit
   val mockConnector: LibraryConnector = mock[LibraryConnector]
   implicit val executionContext: ExecutionContext = app.injector.instanceOf[ExecutionContext]
   val testService = new LibraryService(mockConnector)
+
+  val gameOfThrones: JsValue = Json.obj(
+    "_id" -> "someId",
+    "name" -> "A Game of Thrones",
+    "description" -> "The best book!!!",
+    "pageCount" -> 100
+  )
+
+
 }
