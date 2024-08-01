b0ee251285acaefe6e708bc1cf6e4b4b286070c0
Tayamul <rai_tayamul@hotmail.com>
2024-08-01 18:32:56 +0100

    create a test specification for the LibraryService class and ensure it inherits from the necessary classes and traits

---
+++ test/services/LibraryServiceSpec.scala
@@ -0,0 +1,10 @@
+package services
+
+import baseSpec.BaseSpec
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+
+class LibraryServiceSpec extends BaseSpec with MockFactory with ScalaFutures with GuiceOneServerPerSuite {
+
+}
