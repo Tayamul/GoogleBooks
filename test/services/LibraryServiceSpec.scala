174c1b7b58181ca57c441381c3491ec9aa637f19
Tayamul <rai_tayamul@hotmail.com>
2024-08-01 19:47:14 +0100

    implement a happy path test to ensure the LibraryService returns an object in the form of the Book object

--- test/services/LibraryServiceSpec.scala
+++ test/services/LibraryServiceSpec.scala
@@ -2,12 +2,13 @@ package services
 
 import baseSpec.BaseSpec
 import connectors.LibraryConnector
+import models.Book
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsValue, Json, OFormat}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class LibraryServiceSpec extends BaseSpec with MockFactory with ScalaFutures with GuiceOneServerPerSuite {
 
@@ -22,5 +23,19 @@ class LibraryServiceSpec extends BaseSpec with MockFactory with ScalaFutures wit
     "pageCount" -> 100
   )
 
+  "getGoogleBook" should {
+    val url: String = "testUrl"
+
+    "return a book" in {
+      (mockConnector.get[Book](_: String)(_: OFormat[Book], _: ExecutionContext))
+        .expects(url, *, *)
+        .returning(Future(gameOfThrones.as[Book]))
+        .once()
+
+      whenReady(testService.getGoogleBook(urlOverride = Some(url), search = "", term = "")) { result =>
+        result shouldBe gameOfThrones.as[Book]
+      }
+    }
+  }
 
 }
