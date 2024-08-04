b392a9b5c4737f1d9068e1a2b7d53ca90f7d97b1
Tayamul <rai_tayamul@hotmail.com>
2024-08-04 22:53:10 +0100

    update the error message to improve its flow and clarity

--- app/models/APIError.scala
+++ app/models/APIError.scala
@@ -12,7 +12,7 @@ object APIError {
   final case class BadAPIResponse(upstreamStatus: Int, upstreamMessage: String)
   extends APIError(
     Status.INTERNAL_SERVER_ERROR,
-    s"Bad response from upstream; got status: $upstreamStatus, and got reason $upstreamMessage"
+    s"Received an error from upstream: Status = $upstreamStatus, Reason = $upstreamMessage"
   )
 
 }
