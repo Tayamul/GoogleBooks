cdf2c8df43ff99845257619845535b5165b08663
Tayamul <rai_tayamul@hotmail.com>
2024-08-06 21:54:53 +0100

    comment the get method for the time being again as it's causing an error

--- app/services/LibraryService.scala
+++ app/services/LibraryService.scala
@@ -11,8 +11,8 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class LibraryService @Inject()(connector: LibraryConnector, dataRepository: DataRepository) {
 
-  def getGoogleBook(urlOverride: Option[String] = None, search: String, term: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, Book] =
-    connector.get[Book](urlOverride.getOrElse(s"https://www.googleapis.com/books/v1/volumes?q=$search%$term"))
+//  def getGoogleBook(urlOverride: Option[String] = None, search: String, term: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, Book] =
+//    connector.get[Book](urlOverride.getOrElse(s"https://www.googleapis.com/books/v1/volumes?q=$search%$term"))
 
   def getGoogleBookByIsbn(isbn: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, DataModel] = {
     connector.getBookByIsbn(isbn)
