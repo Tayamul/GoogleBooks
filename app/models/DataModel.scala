402eeaced92a23d715c9fc0bf37df81b940cf038
Tayamul <rai_tayamul@hotmail.com>
2024-08-07 09:14:12 +0100

    create a DataModel form

--- app/models/DataModel.scala
+++ app/models/DataModel.scala
@@ -1,15 +1,28 @@
 package models
 
 import play.api.libs.json.{Json, OFormat}
+import play.api.data._
+import play.api.data.Forms._
+
 
 case class DataModel(
                       _id: String,
                      name: String,
-                     description: Option[String],
-                     pageCount: Option[Int]
+                     description: String,
+                     pageCount: Int
                     )
 
 object DataModel {
   // this allows for easily transforming the model to and from JSON
   implicit val formats: OFormat[DataModel] = Json.format[DataModel]
+
+  val dataForm: Form[DataModel] = Form(
+    mapping(
+      "_id" -> text,
+      "name" -> text,
+      "description" -> text,
+      "pageCount" -> number
+    )(DataModel.apply)(DataModel.unapply)
+  )
+
 }
